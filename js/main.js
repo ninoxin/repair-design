df9a209752a263ea75b662e6e82c6d38f26ec14d
ninoxin <ninoxin2003@mail.ru>
2021-04-09 09:35:32 +0300

    feat: modal window

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    var modal = $('.modal'),
+        modalBtn = $('[data-toggle="modal"]'),
+        closeBtn = $('.modal__close');
+
+    modalBtn.on('click', function () {
+        modal.toggleClass('modal--visible');
+    });
+    closeBtn.on('click', function () {
+        modal.toggleClass('modal--visible');
+    });
+});
+
+$(document).keyup(function (e) {
+    if (e.key === "Escape" || e.keyCode === 27) {
+        modal.toggleClass('modal--visible');
+    }
+});
